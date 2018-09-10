370c2d95b4d129d009b07baa6057e88314d71b3e
jkavdev <jhonatankolen@gmail.com>
2018-09-10 19:53:20 -0300

    implementando menu

--- fontes/groups-manager-ui/src/app/core/header/header.component.ts
+++ fontes/groups-manager-ui/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  menuVisible = "";
+
+  constructor(@Inject(DOCUMENT) private document) {
   }
 
   ngOnInit() {
+  }
+
+  menu() {
+    console.log('passei aqui')
+    var body = this.document.getElementsByClassName('single');
+    if (this.menuVisible.length < 0)
+      body.className = this.exibirMenu();
+    else
+      body.className = this.fecharMenu();
+  }
+
+  exibirMenu() {
+    this.menuVisible = 'is-menu-visible';
+  }
+
+  fecharMenu() {
+    this.menuVisible = '';
   }
 
 }
