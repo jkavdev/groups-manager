9893a44240957f281826bbd8bb3739fe22c11ff9
jkavdev <jhonatankolen@gmail.com>
2018-09-21 16:59:02 -0300

    adicionando mensagem ao realizar busca de integrante

--- fontes/groups-manager-uin/src/app/integrantes/integrantes-pesquisa/integrantes-pesquisa.component.ts
+++ fontes/groups-manager-uin/src/app/integrantes/integrantes-pesquisa/integrantes-pesquisa.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 
+import {MessageService} from 'primeng/api';
 import {IntegranteService} from '../integrante.service';
 
 import {IntegranteFilter} from '../../core/filters';
@@ -7,14 +8,16 @@ import {IntegranteFilter} from '../../core/filters';
 @Component({
   selector: 'app-integrantes-pesquisa',
   templateUrl: './integrantes-pesquisa.component.html',
-  styleUrls: ['./integrantes-pesquisa.component.css']
+  styleUrls: ['./integrantes-pesquisa.component.css'],
+  providers: [MessageService]
 })
 export class IntegrantesPesquisaComponent implements OnInit {
 
   filtro = new IntegranteFilter();
   integrantes = [];
 
-  constructor(private integranteService: IntegranteService) {
+  constructor(private integranteService: IntegranteService,
+              private msgService: MessageService) {
   }
 
   ngOnInit() {
@@ -23,7 +26,11 @@ export class IntegrantesPesquisaComponent implements OnInit {
 
   pesquisarIntegrantes() {
     this.integranteService.pesquisar(this.filtro)
-      .then(integrantes => this.integrantes = integrantes);
+      .then(integrantes => {
+        this.integrantes = integrantes;
+        this.msgService.add({severity: 'info', summary: `Sucesso!`, detail: `Busca realizada!`});
+      })
+      .catch(erro => this.msgService.add({severity: 'error', summary: erro, detail: 'Erro ao realizar busca!'}));
   }
 
 }
