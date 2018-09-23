5a0e9e1f055d9011a1e97945cf2ffea5001392d6
jkavdev <jhonatankolen@gmail.com>
2018-09-23 18:54:54 -0300

    alguns ajustes

--- fontes/groups-manager/src/main/java/br/com/jkavdev/groups/GroupsManagerApplication.java
+++ fontes/groups-manager/src/main/java/br/com/jkavdev/groups/GroupsManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GroupsManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroupsManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GroupsManagerApplication.class, args);
+    }
 }
---
+++ fontes/groups-manager/src/main/java/br/com/jkavdev/groups/event/listener/PropertiesLoggerListener.java
@@ -0,0 +1,55 @@
+package br.com.jkavdev.groups.event.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.PropertySource;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PropertiesLoggerListener implements ApplicationListener<ApplicationPreparedEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(PropertiesLoggerListener.class);
+
+    private ConfigurableEnvironment environment;
+
+    @Override
+    public void onApplicationEvent(ApplicationPreparedEvent event) {
+        environment = event.getApplicationContext().getEnvironment();
+//        evitando que exibar as informacoes do application.properties
+//        printProperties();
+    }
+
+    public void printProperties() {
+        for (EnumerablePropertySource propertySource : findPropertiesPropertySources()) {
+            log.info("********** " + propertySource.getName() + " **********");
+            String[] propertyNames = propertySource.getPropertyNames();
+            Arrays.sort(propertyNames);
+            for (String propertyName : propertyNames) {
+                String resolvedProperty = environment.getProperty(propertyName);
+                String sourceProperty = propertySource.getProperty(propertyName).toString();
+                if (resolvedProperty.equals(sourceProperty)) {
+                    log.info("{}={}", propertyName, resolvedProperty);
+                } else {
+                    log.info("{}={} OVERRIDEN to {}", propertyName, sourceProperty, resolvedProperty);
+                }
+            }
+        }
+    }
+
+    private List<EnumerablePropertySource> findPropertiesPropertySources() {
+        List<EnumerablePropertySource> propertiesPropertySources = new LinkedList<>();
+        for (PropertySource<?> propertySource : environment.getPropertySources()) {
+            if (propertySource instanceof EnumerablePropertySource) {
+                propertiesPropertySources.add((EnumerablePropertySource) propertySource);
+            }
+        }
+        return propertiesPropertySources;
+    }
+
+}
